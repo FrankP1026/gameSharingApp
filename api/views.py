dd2f0872aff6b47cc4351424774afc425e80fbd9
Frank Pan <ruipan1026@gmail.com>
2018-07-08 14:57:23 -0400

    added view for Games

--- api/views.py
+++ api/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
 
+from .models import Games
+from .serializers import GamesSerializer
+
 # Create your views here.
+class ListGamesView(generics.ListAPIView):
+    queryset = Games.objects.all()
+    serializer_class = GamesSerializer
