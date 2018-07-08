8faad6d6c37c3133df64835ca346abb4658d94c3
Frank Pan <ruipan1026@gmail.com>
2018-07-08 13:49:33 -0400

    initialization testing work on models and serializers

--- api/models.py
+++ api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Game(models.Model):
+class Games(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('created_at',)
