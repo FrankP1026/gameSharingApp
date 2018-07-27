607419783e72d4fd163d0b92c5bba7200f423e35
Coral He <che@klick.com>
2018-07-27 01:10:09 -0400

    test commit from cocoral

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className='container'>
+      <div className='app-container'>
         <Header />
         <AddGameForm />
         <GameInBank />
--- src/js/components/presentational/Header.js
+++ src/js/components/presentational/Header.js
@@ -6,12 +6,14 @@ import '../../../scss/components/header.scss';
 const Header = () => {
 	return (
   <header className="header">
-  	<span >Hi <span>User</span>, Share your game! </span>
-  	<ul className="float-right nav">
-	  	<li className="nav-item"><a href="/my-stuff">My stuff </a></li>
-	  	<li className="nav-item"><a href="/my-account">My account </a></li>
-	  	<li className="nav-item"><a href="/log-out">Log out </a></li>
-  	</ul>
+  	<div className="container">
+      <span >Hi <span>User</span>, Share your game! </span>
+      <ul className="float-right nav">
+            <li className="nav-item"><a href="/my-stuff">My stuff </a></li>
+            <li className="nav-item"><a href="/my-account">My account </a></li>
+            <li className="nav-item"><a href="/log-out">Log out </a></li>
+      </ul>
+    </div>
   </header>
   )
 };
